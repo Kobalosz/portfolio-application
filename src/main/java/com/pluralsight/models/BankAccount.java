c95b29ae1569323ec9f880496ddecfed7d709fa1
Kobalosz <imanuel.dartey@gmail.com>
2026-05-19 11:18:21 -0700

    initial commit

---
+++ src/main/java/com/pluralsight/models/BankAccount.java
@@ -0,0 +1,52 @@
+package com.pluralsight.models;
+
+import com.pluralsight.interfaces.Valuable;
+
+public class BankAccount implements Valuable {
+    private String name;
+    private String accountNumber;
+    private double balance;
+
+    public BankAccount(String name, String accountNumber, double balance) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(double amount){
+        this.balance += amount;
+    }
+
+    public void withdraw(double amount){
+        this.balance -= amount;
+    }
+
+    @Override
+    public double getValue() {
+        return balance;
+    }
+}
